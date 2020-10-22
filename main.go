4687f92485504b871352501141ae7aaf0ba44877
Micks1317 <73315177+Micks1317@users.noreply.github.com>
2020-10-22 17:31:03 -0400

    Updated Message

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "What's Up Everyone"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
